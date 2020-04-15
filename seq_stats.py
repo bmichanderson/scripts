1c19ff89cc2b038bdb86781c0a181585c292d1b5
bmichanderson <banderson2914@gmail.com>
2020-04-15 13:48:45 +0200

    created first version of seq_stats.py

---
+++ seq_stats.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+
+#################
+# Script: seq_stats.py
+# Author: B. Anderson
+# Date: 15 Apr 2020
+# Description: output basic sequence statistics for a set of input fasta files
+#################
+
+import sys
+from Bio import SeqIO
+from Bio.SeqUtils import GC
+from Bio.Seq import Seq
+
+def help():
+	print('A script to output basic sequence statistics for a set of input fasta files')
+	print('')
+	print('Usage: ' + str(sys.argv[0]) + ' fasta1 fasta2 fasta3 ...')
+	print('')
+
+
+# print help if the script is called without arguments
+if len(sys.argv[1:]) == 0:
+	sys.exit(help())
+
+# for each of the input files, read the sequence(s) then calculate and output stats
+cum_seqs = []
+for fasta in sys.argv[1:]:
+	print(fasta)
+
+	fsa = SeqIO.parse(open(fasta, 'r'), 'fasta')
+	entries = []
+	for entry in fsa:
+		entries.append(entry)
+
+	seqs = []
+	for entry in entries:
+		seqs.append(entry.seq)
+		print('ID: ' + entry.id)
+		n_count = entry.seq.count('N') + entry.seq.count('n')
+		print('Length: ' + str(len(entry.seq)) + ', GC content: ' + ('%.3f' % GC(entry.seq)) + ', proportion Ns: ' + str(100*n_count/len(entry.seq)))
+
+
+	cum_seq = Seq('').join(seqs)
+	cum_seqs.append(cum_seq)
+	n_count = cum_seq.count('N') + cum_seq.count('n')
+	print('Cumulative length: ' + str(len(cum_seq)) + ', GC content: ' + ('%.3f' % GC(cum_seq)) + ', proportion Ns: ' + str(100*n_count/len(cum_seq)))
+	print('')
+
+overall_seq = Seq('').join(cum_seqs)
+n_count = overall_seq.count('N') + overall_seq.count('n')
+print('Overall length: ' + str(len(overall_seq)) + ', GC content: ' + ('%.3f' % GC(overall_seq)) + ', proportion Ns: ' + str(100*n_count/len(overall_seq)))
