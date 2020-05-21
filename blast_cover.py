eb384d47b36490399da834c64372cb28a2c447fc
bmichanderson <banderson2914@gmail.com>
2020-05-21 17:20:11 +0200

    converted parse_blastn-5.1.py to blastn_parse.py; changed blast_cover.py

--- blast_cover.py
+++ blast_cover.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python
 
 #####################
-# Script: blast_cover.py
 # Author: B. Anderson
-# Date: 15 Apr 2020
-# Description: parse a plot.tab file from parse_blastn to calculate the percentage cover
+# Date: 15 Apr 2020, 21 May 2020 (updated to now handle the more BED-like output of the new blastn_parse script)
+# Description: parse a plot_data.tab file from blastn_parse to calculate the percentage cover
 #####################
 
 import sys
 
+
 def help():
-	print('A script to parse a plot.tab file from parse_blastn to calculate the percentage cover')
+	print('A script to parse a plot_data.tab file from parse_blastn to calculate the percentage cover')
 	print('')
-	print('Usage: ' + sys.argv[0] + ' plot.tab')
+	print('Usage: ' + sys.argv[0] + ' plot_data.tab')
 	print('')
 
 
@@ -21,7 +21,7 @@ if (len(sys.argv[1:])) == 0:
 	sys.exit(help())
 
 
-# read in the plot.tab file line by line and capture the hit information
+# read in the plot_data.tab file line by line and capture the hit information
 hit_dict = {}
 sbjct_len_dict = {}
 cover_dict = {}
@@ -55,9 +55,12 @@ with open(sys.argv[1], 'r') as input:
 
 		# capture the hit information by subject
 		if sbjct not in hit_dict:
-			hit_dict[sbjct] = [sorted((sbjct_start, sbjct_end))]
+			#hit_dict[sbjct] = [sorted((sbjct_start, sbjct_end))]
+			# NOTE: since these are now proper BED coordinates, the actual base positions are automatically lesser to greater and 0-based and non-inclusive of end position
+			hit_dict[sbjct] = [(sbjct_start, sbjct_end)]
 		else:
-			hit_dict[sbjct].append(sorted((sbjct_start, sbjct_end)))
+			#hit_dict[sbjct].append(sorted((sbjct_start, sbjct_end)))
+			hit_dict[sbjct].append((sbjct_start, sbjct_end))
 
 
 		# capture the subject lengths and sbjct/query presence
@@ -75,14 +78,16 @@ with open(sys.argv[1], 'r') as input:
 for sbjct in hit_dict:
 	hit_ranges = []
 	for begin, end in sorted(hit_dict[sbjct]):		# from https://stackoverflow.com/questions/15273693/union-of-multiple-ranges
-		if hit_ranges and hit_ranges[-1][1] >= begin - 1:
+		#if hit_ranges and hit_ranges[-1][1] >= begin - 1:
+		if hit_ranges and hit_ranges[-1][1] >= begin:		# had to change this since BED ranges don't include the last position
 			hit_ranges[-1][1] = max(hit_ranges[-1][1], end)
 		else:
 			hit_ranges.append([begin, end])
 
 	total_length = 0
 	for begin, end in hit_ranges:
-		total_length = total_length + len(range(begin, end + 1))
+		#total_length = total_length + len(range(begin, end + 1))
+		total_length = total_length + len(range(begin, end))		# another change since BED lengths are the same as range lengths (i.e. end - begin)
 
 	if sbjct not in cover_dict:
 		cover_dict[sbjct] = total_length
@@ -100,7 +105,7 @@ for sbjct in sbjct_len_dict:
 
 print('Summary for BLAST results with ' + str(len(sbjct_len_dict)) + ' subjects and ' + str(len(query_list)) + ' queries.')
 print('')
-print('Hit coveraged:')
+print('Hit coverage:')
 for sbjct in sorted(sbjct_list):
 	if sbjct in cover_dict:
 		print(sbjct + ': ' + str(cover_dict[sbjct]) + ' of ' + str(sbjct_len_dict[sbjct]))
