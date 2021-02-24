d27f197c513175240e9c77c646c190438acb450a
bmichanderson <banderson2914@gmail.com>
2021-02-24 02:43:30 +0100

    updated ncbi_search.py

--- ncbi_search.py
+++ ncbi_search.py
@@ -3,91 +3,73 @@
 ##########################
 # Author: B. Anderson
 # Date: 25 Feb 2020
-# Modified: Oct 2020
+# Modified: Oct 2020, Feb 2021
 # Description: search NCBI Genbank databases and report a summary for later use in downloading
 ##########################
 
+import argparse
 import sys			# allows access to command line arguments
 from Bio import Entrez		# for interacting with the online Entrez databases
 
-def help():
-	print('A script to search Genbank and provide a summary with accession numbers for later downloading.')
-	print('')
-	print('Usage: ' + str(sys.argv[0]) + ' options(- ... - ...) search_string')
-	print('')
-	print('Options:')
-	print('	-f	Whether to run the full summary, yes or no [default]')
-	print('')
-	print('	-m	Maximum number of records to return/search [default 50]')
-	print('')
-	print('	-t	Whether to search for and report taxonomy, yes or no [default]')
-	print('')
-	print('The search string should be in parentheses, e.g. \"Cuscuta[Organism] AND mitochondrial[Title]\"')
-	print('')
+
+# instantiate the parser
+parser = argparse.ArgumentParser(description = 'A script to search Genbank and provide a summary with accession numbers for later downloading')
 
 
-options = ['-f', '-m', '-t']
-opt_valdic = {}
+# add arguments to parse
+parser.add_argument('search_str', type=str, help='The search string to submit to NCBI; put in quotations: \" \"')
+parser.add_argument('-f', dest='full', action='store_true', help='Run the full summary and output to file [default: do not]')
+parser.add_argument('-m', type=int, dest='max_recs', help='Specify the maximum number of records to return [default 50]')
+parser.add_argument('-t', dest='tax', action='store_true', help='Retrieve higher taxonomic info for the records on a full summary [default: do not]')
+parser.set_defaults(full=False)
+parser.set_defaults(tax=False)
 
 
-# Parse the command line
-if len(sys.argv[1:]) > 0:
+# parse the command line
+if len(sys.argv[1:]) == 0:		# if there are no arguments
+	parser.print_help(sys.stderr)
+	sys.exit(1)
 
-	search_string = sys.argv[-1:][0]		# retrieve the last argument slice, then entry as string
+args = parser.parse_args()
 
-	for index, arg in enumerate(sys.argv[1:]):
-		if arg[0] == '-': 			# if an option
-			if arg in options:
-				if sys.argv[1:][index + 1][0] == '-':
-					sys.exit('No argument provided for ' + str(arg))
-				opt_valdic[arg] = sys.argv[1:][index + 1]
-			else:
-				sys.exit('The argument ' + str(arg) + ' is not a valid option')
+search_str = args.search_str
+full = args.full
+max_recs = args.max_recs
+tax = args.tax
 
-	if '-f' in opt_valdic:
-		if opt_valdic['-f'].lower() == 'yes':
-			full_summ = True
-	else:
-		full_summ = False
+if not max_recs:
+	max_recs = 50
 
-	if '-m' in opt_valdic:
-		max_records = int(opt_valdic['-m'])
-	else:
-		max_records = 50
-
-	if '-t' in opt_valdic:
-		if opt_valdic['-t'].lower() == 'yes':
-			search_tax = True
-	else:
-		search_tax = False
-
-else:
-	sys.exit(help())
+if full:
+	print('Running a full summary and outputting result to search_summary.tab')
+elif tax:
+	sys.exit('The taxonomy -t option only works with a full summary -f option')
 
 
-# perform the search and retrieve accession numbers
+# Perform the search and retrieve accession numbers
 Entrez.email = 'benjamin.anderson@su.se'		# so they can notify me if my usage is improper; this is required
 
-handle = Entrez.esearch(db = 'nucleotide', term = search_string, idtype = 'acc', retmax = max_records)
+handle = Entrez.esearch(db = 'nucleotide', term = search_str, idtype = 'acc', retmax = max_recs)
 record = Entrez.read(handle)
 handle.close()
 
-if int(record['Count']) > max_records:		# this may be different from the number of accession numbers returned (note: default retmax = 20)
-	print('Note that ' + record['Count'] + ' records were found, but only ' + str(max_records) + ' are set to be retrieved.')
+if int(record['Count']) > max_recs:		# this may be different from the number of accession numbers returned (note: default retmax = 20)
+	print('Note that ' + record['Count'] + ' records were found, but only ' + str(max_recs) + ' are set to be retrieved')
 
 acc_list = record['IdList']
 
-print('Retrieved ' + str(len(acc_list)) + ' accession numbers for the input search')
+print('Retrieved ' + str(len(acc_list)) + ' accession numbers for the input search string')
 
-if not full_summ:
+
+# Report result and optionally retrieve and output additional information to file
+if not full:
 	print('Accession numbers:')
 	for acc_num in acc_list:
 		print(acc_num)
-
-
-else:		# For each accession number, retrieve the record details and organism info to prepare the summary
+else:
 	rec_list = []
 	genera_list = []
+	genera_search = []
 	count = len(acc_list)
 
 	if count > 50:
@@ -98,26 +80,30 @@ else:		# For each accession number, retrieve the record details and organism inf
 	for start in range(0, count, batch_size):
 		end = min(count, start + batch_size)
 		print('Searching Genbank accession details for ' + str(start + 1) + ' to ' + str(end) + ' of ' + str(count))
-		for position in range(start, end):
-			handle = Entrez.esummary(db = 'nucleotide', id = acc_list[position])
-			records = Entrez.read(handle)
-			handle.close()
 
-			title = records[0]['Title']
+		sub_acc_list = acc_list[start: end]
+		id_list = ','.join(sub_acc_list)	# comma-delimited list of accession numbers
+		handle = Entrez.esummary(db = 'nucleotide', id = id_list)
+		records = Entrez.read(handle)
+		handle.close()
+
+		for index, record in enumerate(records):
+			title = record['Title']
 			genus = title.split()[0]
 			specific_ep = title.split()[1]
-			length = str(records[0]['Length'])
-			tax_id = records[0]['TaxId']
+			length = str(record['Length'])
+			tax_id = record['TaxId']
 
 			if genus not in genera_list:
-				genera_list.append((genus, tax_id))
+				genera_list.append(genus)
+				genera_search.append((genus, tax_id))
 
-			rec_list.append((genus, specific_ep, acc_list[position], length, title))
+			rec_list.append((genus, specific_ep, sub_acc_list[index], length, title))
 
 	# Use the taxonomy database to extract order and family for each genus, if requested
-	if search_tax:
+	if tax:
 		genera_dic = {}
-		count = len(genera_list)
+		count = len(genera_search)
 
 		if count > 50:
 			batch_size = 50
@@ -127,26 +113,26 @@ else:		# For each accession number, retrieve the record details and organism inf
 		for start in range(0, count, batch_size):
 			end = min(count, start + batch_size)
 			print('Searching Genbank taxonomy details for ' + str(start + 1) + ' to ' + str(end) + ' of ' + str(count) + ' genera')
-			for position in range(start, end):
-				tax_id = genera_list[position][1]
-				handle = Entrez.efetch(db = 'Taxonomy', id = tax_id)
-				records = Entrez.read(handle)
-				handle.close()
 
-				for level in records[0]['LineageEx']:
+			sub_gen_list = genera_search[start: end]
+			id_list = ','.join([str(x[1]) for x in sub_gen_list])
+			handle = Entrez.efetch(db = 'Taxonomy', id = id_list)
+			records = Entrez.read(handle)
+			handle.close()
+
+			for index, record in enumerate(records):
+				for level in record['LineageEx']:
 					if level['Rank'] == 'order':
 						order = level['ScientificName']
 					elif level['Rank'] == 'family':
 						family = level['ScientificName']
 					else:
 						continue
-
-				genera_dic[genera_list[position][0]] = (order, family)
-
+				genera_dic[sub_gen_list[index][0]] = (order, family)
 
 	# Output a summary
 	out_list = []
-	if search_tax:
+	if tax:
 		for rec in rec_list:
 			out_list.append('\t'.join(genera_dic[rec[0]]) + '\t' + '\t'.join(rec))
 	else:
@@ -154,8 +140,7 @@ else:		# For each accession number, retrieve the record details and organism inf
 			out_list.append('\t'.join(rec))
 
 	with open('search_summary.tab', 'w') as outfile:
-		if search_tax:
+		if tax:
 			outfile.write('\t'.join(('order', 'family', 'genus', 'specific_ep', 'accession', 'length', 'full_title')) + '\n' + '\n'.join(out_list) + '\n')
 		else:
 			outfile.write('\t'.join(('genus', 'specific_ep', 'accession', 'length', 'full_title')) + '\n' + '\n'.join(out_list) + '\n')
-
