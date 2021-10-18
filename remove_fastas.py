8aa6accc9eb9063a653d297d7934eaf62fad6ade
bmichanderson <banderson2914@gmail.com>
2021-10-18 19:11:02 +0800

    added support to take an input file of strings to match for removal

--- remove_fastas.py
+++ remove_fastas.py
@@ -2,57 +2,71 @@
 ###############################################
 # Author: B. Anderson
 # Date: 28 Jun 2019
-# Description: a script to simply remove entries from multifastas based on a string matches (arg1)
+# Updated: Oct 2021 (can now take a text file with strings for string matching, one per line; also cleaned up and added parser)
+# Description: a script to simply remove entries from multifastas based on string matches (arg1)
 ###############################################
 
 import sys
+import argparse
 from Bio import SeqIO
 
-def help():
-	print('A script to remove fasta entries based on string matching.')
-	print('The new fasta files are output in the current directory.')
-	print('')
-	print('Usage: ' + str(sys.argv[0]) + ' string1,string2,... multifasta1 multifasta2 ...')
-	print('')
 
-# print help if the script is called without args
-if len(sys.argv[1:]) == 0:
-	sys.exit(help())
+# instantiate the parser
+parser = argparse.ArgumentParser(description = 'A script to remove fasta entries based on string matching. ' +
+												'The new fasta files are output in the current directory with prefix \"mod_\".')
+
+
+# add arguments to parse
+parser.add_argument('strings', type = str, help = 'The comma delimited strings to search for when removing fastas from the multifastas (or nothing if file specified), ' +
+					'and then the space-delimited multifastas', nargs = '*')
+parser.add_argument('-f', type = str, dest = 'string_file', help = 'A text file with a list of strings, one per line (optional). ' + 
+					'If not specified, then the first space-delimited command line argument will be interpreted as the comma-delimited search strings.')
 
 
 # parse the command line
-strings = sys.argv[1]
-string_list = []
-for part in strings.split(','):
-	string_list.append(part)
+if len(sys.argv[1:]) == 0:		# if there are no arguments
+	parser.print_help(sys.stderr)
+	sys.exit(1)
 
+args = parser.parse_args()
+
+strings = args.strings
+string_file = args.string_file
+
+if not strings:
+	parser.print_help(sys.stderr)
+	sys.exit(1)
+
+string_list = []
 file_list = []
-for arg in sys.argv[2:]:
-	file_list.append(arg)
+if not string_file:			# if there is no file specified with '-f'
+	search_strings = strings[0]
+	for part in search_strings.split(','):
+		string_list.append(part)
+	for fasta in strings[1:]:
+		file_list.append(fasta)
+else:
+	with open(string_file, 'r') as str_file:
+		for line in str_file:
+			string_list.append(line.rstrip())
+	for fasta in strings:
+		file_list.append(fasta)
 
 
 # read in the each multifasta file and remove entries
 for multifasta in file_list:
-
 	fasta_list = []
-
 	fastas = SeqIO.parse(open(multifasta, 'r'), 'fasta')
-
 	for fasta in fastas:
 		fasta_list.append(fasta)
-
 	remove_indices = []
-
-	for i,fasta in enumerate(fasta_list):
+	for i, fasta in enumerate(fasta_list):
 		for string in string_list:
 			if string in fasta.description:
 				remove_indices.append(i)
-
 	for i in sorted(remove_indices, reverse = True):
 		del fasta_list[i]
-
 	print('Removed ' + str(len(remove_indices)) + ' entries from ' + multifasta)
-
 	with open('mod_' + multifasta, 'w') as outfile:
 		for fasta in fasta_list:
 			SeqIO.write(fasta, outfile, 'fasta')
