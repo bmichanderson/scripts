da365266f5bc50a89faa7e97dbe919b940685d7d
bmichanderson <banderson2914@gmail.com>
2023-03-07 18:09:31 +0800

    added sequence removal option

--- clean_alignment.py
+++ clean_alignment.py
@@ -3,8 +3,8 @@
 ###############################################
 # Author: B. Anderson
 # Date: 12 Mar 2019
-# Modified: Jun 2021 (updated for phylip and using AlignIO, numpy and pandas), Oct 2020
-# Description: read in an alignment and remove positions with more than a specified number or percentage of gaps or Ns
+# Modified: Mar 2023 (add sequence removal option), Jun 2021 (updated for phylip and using AlignIO, numpy and pandas), Oct 2020
+# Description: read in an alignment and remove positions/sequences with more than a specified number or percentage of gaps or Ns
 ###############################################
 
 
@@ -18,7 +18,8 @@ from Bio.Align import MultipleSeqAlignment
 
 
 def help():
-	print('A script to remove missing positions in an alignment.')
+	print('A script to remove positions/sequences in an alignment based on number/percentage gaps and Ns.')
+	print('One of -g or -p needs to be chosen; -g overrides -p.')
 	print('The cleaned alignment is output as a fasta/phylip file in the current directory.')
 	print('')
 	print('Usage: ' + str(sys.argv[0]) + ' options(-... -...) alignment')
@@ -26,24 +27,24 @@ def help():
 	print('Options:')
 	print('	-f	File type (fasta [default] or phylip)')
 	print('')
-	print('	-g	Maximum number of gaps/Ns in the alignment at any position')
+	print('	-g	Maximum number of gaps and Ns allowed in the alignment at any position')
 	print('')
-	print('	-p	Maximum percentage (%) of gap/N positions in the alignment at any position')
+	print('	-p	Maximum percentage (%) of gaps and Ns allowed in the alignment at any position')
+	print('')
+	print('	-s	Maximum percentage (%) of gaps and Ns allowed in a sequence to keep it in the alignment [default: 100]')
 	print('')
 
 
-opts = ['-f', '-g', '-p']
+opts = ['-f', '-g', '-p', '-s']
 opt_valdic = {}
 
 
 # print help if the script is called without args
-
 if len(sys.argv[1:]) == 0:
 	sys.exit(help())
 
 
 # assign arguments to variables and evaluate options
-
 alignment = sys.argv[-1:][0]		# retrieve the last argument slice, then entry as string
 
 for index, arg in enumerate(sys.argv[1:-1]):	# don't include last entry
@@ -60,7 +61,6 @@ for index, arg in enumerate(sys.argv[1:-1]):	# don't include last entry
 	else:
 		continue
 
-
 if '-f' in opt_valdic:
 	align_specified = True
 	align_type = opt_valdic['-f']
@@ -68,13 +68,11 @@ else:
 	align_specified = False
 	align_type = 'fasta'
 
-
 if '-g' in opt_valdic:
 	gap_number = True
 	num_gaps = int(opt_valdic['-g'])
 else:
 	gap_number = False
-
 
 if '-p' in opt_valdic:
 	gap_percent = True
@@ -82,17 +80,18 @@ if '-p' in opt_valdic:
 else:
 	gap_percent = False
 
+if '-s' in opt_valdic:
+	seq_percent = True
+	seq_perc_gaps = int(opt_valdic['-s'])
+else:
+	seq_percent = False
+
 
 # read in the alignment
-
 if align_type == 'fasta':
-
 	align = AlignIO.read(open(alignment, 'r'), 'fasta')
-
 elif align_type == 'phylip':
-
 	align = AlignIO.read(open(alignment, 'r'), 'phylip-relaxed')
-
 else:
 	sys.exit('Please specify alignment type as "fasta" or "phylip"')
 
@@ -105,7 +104,8 @@ for sequence in align:
 	descriptions.append(sequence.description)
 
 len_align = align.get_alignment_length()
-print('Alignment length is: ' + str(len_align))
+print('Alignment length is: ' + str(len_align) + ' bp')
+print('Number of sequences: ' + str(len(align)))
 
 
 # now that the data has been read, we need to convert the sequences to an array, then to a dataframe
@@ -116,8 +116,7 @@ def upperit(x):
 	return(str(x).upper())
 
 
-# if the alignment is super large
-if len_align > 500000:
+if len_align > 500000:	# alignment is super large
 	align_list = []
 	increment = 200000
 
@@ -127,30 +126,23 @@ if len_align > 500000:
 		print('Filtering from positions: ' + str(start) + ' to ' + str(end) + ' of ' + str(len_align))
 
 		n_array = numpy.array([list(sample) for sample in align[:, start: end]])
-
 		p_df = pandas.DataFrame(n_array)
 		rows = float(len(p_df))
 
 		# convert to upper case for search
-
 		p_df = p_df.applymap(upperit)
 
 		# now we need to filter the dataframe by columns, to keep columns which pass filters
 		# from https://stackoverflow.com/questions/31614804/how-to-delete-a-column-in-pandas-dataframe-based-on-a-condition/31618820
-
 		if gap_number:
 			filtp_df = p_df.loc[:, (p_df.eq('-').sum() + p_df.eq('N').sum() <= num_gaps)]
-
 		elif gap_percent:
 			filtp_df = p_df.loc[:, (round(100*(p_df.eq('-').sum() + p_df.eq('N').sum())/rows, 0) <= perc_gaps)]
-
 		else:
 			sys.exit('Specify gap/N threshold')
 
 		# now that the dataframe is filtered, we need to convert it back into an alignment
-
 		new_records = []
-
 		for index, sequence in enumerate(filtp_df.values.tolist()):
 			new_sequence = Seq(''.join(sequence))
 			new_record = SeqRecord(new_sequence, id = ids[index], description = descriptions[index])
@@ -161,72 +153,72 @@ if len_align > 500000:
 
 
 	# stick the alignments together and output the new alignment
-
 	new_alignment = align_list[0]
 	for piece in align_list[1:]:
 		new_alignment = new_alignment + piece
 
+	# if there is a flag to check gap composition of sequences, do so
+	if seq_percent:
+		temp_record_list = []
+		for align_rec in new_alignment:
+			this_count = align_rec.seq.count('-') + align_rec.seq.count('N')
+			if 100 * float(this_count) / len(align_rec.seq) > seq_perc_gaps:
+				continue
+			
+			temp_record_list.append(align_rec)
+
+		new_alignment = MultipleSeqAlignment(temp_record_list)
+
+
 	print('Filtered alignment is ' + str(new_alignment.get_alignment_length()) + ' bp')
+	print('Filtered alignment contains ' + str(len(align)) + ' sequences')
 
 	if align_type == 'fasta':
-
 		with open(alignment.replace('.f', '_clean.f'), 'w') as out_file:
 			AlignIO.write(new_alignment, out_file, 'fasta')
-
 	elif align_type == 'phylip':
-
 		with open(alignment.replace('.p', '_clean.p'), 'w') as out_file:
 			AlignIO.write(new_alignment, out_file, 'phylip-relaxed')
 
-
-else:
+else:		# alignment is reasonable size
 	n_array = numpy.array([list(sample) for sample in align])
 	print('Dimensions of array: ' + str(n_array.shape))
-
 	p_df = pandas.DataFrame(n_array)
 	rows = float(len(p_df))
 
 	# convert to upper case for search
-
 	p_df = p_df.applymap(upperit)
-
 
 	# now we need to filter the dataframe by columns, to keep columns which pass filters
 	# from https://stackoverflow.com/questions/31614804/how-to-delete-a-column-in-pandas-dataframe-based-on-a-condition/31618820
-
 	if gap_number:
 		filtp_df = p_df.loc[:, (p_df.eq('-').sum() + p_df.eq('N').sum() <= num_gaps)]
-
 	elif gap_percent:
 		filtp_df = p_df.loc[:, (round(100*(p_df.eq('-').sum() + p_df.eq('N').sum())/rows, 0) <= perc_gaps)]
-
 	else:
 		sys.exit('Specify gap/N threshold')
 
-	#print('New data frame has ' + str(rows) + ' rows and ' + len(filtp_df.columns) + ' columns')
-
-
 	# now that the dataframe is filtered, we need to convert it back into an alignment
-
 	new_records = []
-
 	for index, sequence in enumerate(filtp_df.values.tolist()):
 		new_sequence = Seq(''.join(sequence))
+		# if there is a flag to check gap composition of sequences, do so
+		if seq_percent:
+			this_count = new_sequence.count('-') + new_sequence.count('N')
+			if 100 * float(this_count) / len(new_sequence) > seq_perc_gaps:
+				continue
+
 		new_record = SeqRecord(new_sequence, id = ids[index], description = descriptions[index])
 		new_records.append(new_record)
 
 	new_alignment = MultipleSeqAlignment(new_records)
-
 	print('Filtered alignment is ' + str(new_alignment.get_alignment_length()) + ' bp')
+	print('Filtered alignment contains ' + str(len(new_alignment)) + ' sequences')
 
 	# output the new alignment
-
 	if align_type == 'fasta':
-
 		with open(alignment.replace('.f', '_clean.f'), 'w') as out_file:
 			AlignIO.write(new_alignment, out_file, 'fasta')
-
 	elif align_type == 'phylip':
-
 		with open(alignment.replace('.p', '_clean.p'), 'w') as out_file:
 			AlignIO.write(new_alignment, out_file, 'phylip-relaxed')
