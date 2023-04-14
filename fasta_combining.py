61ae5cd6108b561ea77f7c2f7d24399010338753
bmichanderson <banderson2914@gmail.com>
2023-04-14 16:37:18 +0800

    cleaned up and added output option

--- fasta_combining.py
+++ fasta_combining.py
@@ -1,48 +1,51 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 ##########################
 # Author: B. Anderson
 # Date: 10 Mar 2020
-# Modified: Oct 2020
+# Modified: April 2023; Oct 2020
 # Description: combine fasta files, removing duplicate sequences
 ##########################
 
+
 import sys
+import argparse
 from Bio import SeqIO		# for reading and writing sequence records
 
-def help():
-	print('A script to combine fasta entries in multiple files into a single file.')
-	print('The fastas are output as a single multifasta file called combined.fasta in the current directory.')
-	print('')
-	print('Usage: ' + str(sys.argv[0]) + ' file1 file2 ...')
-	print('')
+
+# instantiate the parser
+parser = argparse.ArgumentParser(description = 'A script to combine fasta entries in multiple files into a single file.')
 
 
-# print help if the script is called without args
-if len(sys.argv[1:]) == 0:
-	sys.exit(help())
+# add arguments to parse
+parser.add_argument('fasta_files', type=str, help='The fasta files', nargs='*')
+parser.add_argument('-o', type = str, dest = 'out_file', help = 'The output file to create [default: combined.fasta]')
 
 
-# capture list of files
-file_list = []
-for arg in sys.argv[1:]:
-	file_list.append(arg)
+# parse the command line
+if len(sys.argv[1:]) == 0:		# if there are no arguments
+	parser.print_help(sys.stderr)
+	sys.exit(1)
+args = parser.parse_args()
+fasta_files = args.fasta_files
+out_file = args.out_file
+
+if not out_file:
+	out_file = 'combined.fasta'
 
 
 # extract the fastas from the files
 fastas = []
-for f_file in file_list:
+for f_file in fasta_files:
 	with open(f_file, 'r') as fasta_file:
-		fasta_iter = SeqIO.parse(fasta_file, 'fasta')
-		for entry in fasta_iter:
+		for entry in SeqIO.parse(fasta_file, 'fasta'):
 			fastas.append(entry)
 
 
-# remove duplicate entries
+# remove exact duplicate entries
 dlist = []
 seqlist = []
 to_remove = []
-
 for num, fasta in enumerate(fastas):
 	if fasta.description in dlist:
 		d_indices = [i for i in range(len(dlist)) if dlist[i] == fasta.description]
@@ -56,7 +59,6 @@ for num, fasta in enumerate(fastas):
 		if found == 'False':
 			dlist.append(fasta.description)
 			seqlist.append(fasta.seq)
-
 	else:
 		dlist.append(fasta.description)
 		seqlist.append(fasta.seq)
@@ -65,9 +67,7 @@ for index in sorted(to_remove, reverse = True):
 	del fastas[index]
 
 
-# sort the fastas, then output to a new file
-#fastas = sorted(fastas, key=lambda x: len(x.seq), reverse=True)
-
-with open('combined.fasta', 'w') as output_file:
+# output the fastas
+with open(out_file, 'w') as output_file:
 	for entry in fastas:
 		SeqIO.write(entry, output_file, 'fasta')
