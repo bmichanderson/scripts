a455a7e70e1ae80f3f1b413eddb30004193bdec2
bmichanderson <banderson2914@gmail.com>
2023-06-27 17:39:02 +0800

    added filtering and altered output substantially

--- hmmer_parse.py
+++ hmmer_parse.py
@@ -3,102 +3,174 @@
 ##########################
 # Author: B. Anderson
 # Date: 18 May 2020
-# Description: Parse the output of a HMMER search
+# Modified: June 2023
+# Description: Parse the output of a HMMER search, and optionally filter for top/multiple hits
 ##########################
 
 
-import sys			# allows access to command line arguments
+import sys
+import argparse
 
 
-def help():
-	print('A script to parse the results of a hmmsearch of a HMMER profile and a protein sequence(s) file from ORFfinder.')
-	print('')
-	print('Usage: ' + str(sys.argv[0]) + ' hmmer_file.out')
-	print('')
+# instantiate the parser
+parser = argparse.ArgumentParser(description = 'A script to parse the results file of a hmmsearch of a HMMER profile and ' +
+	'a set of protein sequences; e.g., translated output from ORFfinder CDS fasta. If desired, the hits can be filtered ' +
+	'using the \"-t\" argument.')
 
 
-# print help if the script is called without args
-if len(sys.argv[1:]) == 0:
-	sys.exit(help())
+# add arguments to parse
+parser.add_argument('hmmer', type = str, help = 'The HMMER output file')
+parser.add_argument('-t', type = float, dest = 'thresh', help = 'The E-value threshold for filtering, e.g. \"1e-5\"')
+
+
+# parse the command line
+if len(sys.argv[1:]) == 0:		# if there are no arguments
+	parser.print_help(sys.stderr)
+	sys.exit(1)
+args = parser.parse_args()
+hmmer = args.hmmer
+thresh = args.thresh
+
+if thresh:
+	filter_hits = True
+	threshold = thresh
+else:
+	filter_hits = False
+	threshold = 1
 
 
 # parse the hmmer output line by line and capture info into lists
-with open(sys.argv[1], 'r') as hmmer_file, open(sys.argv[1].replace('.out', '') + '_parsed.out', 'w') as out_file:
-
+with open(hmmer, 'r') as hmmer_file, open(hmmer + '.parsed', 'w') as out_file:
 	# first, capture all the hit information
 	capture_hits = []
 	hit_num = 0
 	domain_num = 0
 	index = 0
 
-	for line_no, line in enumerate(hmmer_file, start=1):
-		if line.startswith('#'):
+	for line_no, line in enumerate(hmmer_file, start = 1):
+		if not line.strip():
 			continue
-		elif not line.strip():
-			continue
-		elif line.split()[0] == 'Query:':	# indication of first hit in hmmer database
+		pieces = line.strip().split()
+		if pieces[0] == 'Query:':	# indication of hit in hmmer database
 			query = line.split()[1]
 			mlen = line.split()[2].strip('[').strip(']').split('=')[1]
 			hit_num = 0
-		elif line.startswith('>>'):		# indication of domain hit
-			hit = line.split()[1]
+		elif pieces[0] == '>>':		# indication of domain hit
+			hit = pieces[1]
 			index = line_no
 			hit_num = hit_num + 1
 			domain_num = 1
-		elif all([line.lstrip().split()[0] == str(domain_num), line_no == index + 2 + domain_num]):	# hits are sequential starting 3 lines below >>
+		elif all([pieces[0] == str(domain_num), line_no == index + 2 + domain_num]):	# hits are sequential starting 3 lines below >>
 			# capture hit coordinate information of importance
-			hmm_coord = line.split()[6:9]
-			env_coord = line.split()[12:15]
-			capture_hits.append([query, hit, hit_num, domain_num, hmm_coord, env_coord, mlen])
+			evalue = pieces[5]		# independent domain evalue, not the conditional evalue
+			hmm_coord = pieces[6:9]		# two numbers and a string
+			env_coord = pieces[12:15]	# two numbers and a string
+			capture_hits.append([query, mlen, hit, evalue, hmm_coord, env_coord])
 			domain_num = domain_num + 1
-		elif line.startswith('//'):		# indication of end of hmmer hit
+		elif pieces[0] == '//':		# indication of end of hmmer hit
 			print('Processed query: ' + query)
 		else:
 			continue
 
-
-	# second, format the captured info for output
+	# second, format and output the captured info
 	query_list = []
-
-	out_file.write('Contig\tHit_num\tDomain_num\tHmm_coord\tMlen\tEnv_coord\tORFlen\tEquiv_coord\t\n')
-
-	print(len(capture_hits))
+	out_file.write('Query\tMlen\tContig\tC_len\tE-value\t' +
+		'H_coord_start\tH_coord_end\tH_indicator\tE_coord_start\t' +
+		'E_coord_end\tE_indicator\tC_coord_start\tC_coord_end\t\n')
+	print('Captured ' + str(len(capture_hits)) + ' hits')
 
 	for capture in capture_hits:
-
 		# re-assign variables for clarity
 		query = capture[0]
-		hit = capture[1]
-		hit_num = capture[2]
-		domain_num = capture[3]
+		mlen = capture[1]
+		hit = capture[2]
+		evalue = capture[3]
 		hmm_coord = capture[4]
 		env_coord = capture[5]
-		mlen = capture[6]
 
-		# this is specific to the formatting of ORFfinder output
-		locus = hit.split('_')[1]
-		contig = locus.split(':')[0]
-		coord_start = locus.split(':')[1]
-		coord_end = locus.split(':')[2]
+		# ORFfinder output may contain hit location information
+		locus_parts = hit.split(':')
+		contig = locus_parts[0]
+		if len(locus_parts) > 1:
+			coord_start = locus_parts[1].split('-')[0]
+			coord_end = int(locus_parts[1].split('-')[1])
+			# convert the coordinates to match the envelope coords in terms of contig coords
+			eq_coord = []
+			start_adjust = (int(env_coord[0]) - 1) * 3
+			end_adjust = (int(env_coord[1]) - int(env_coord[0])) * 3
+			if str(coord_start).startswith('c'):		# reverse
+				eq_coord.append(str(int(coord_start.split('c')[1]) - start_adjust))
+				eq_coord.append(str(int(coord_start.split('c')[1]) - start_adjust - end_adjust - 2))
+				orflen = (int(coord_start.split('c')[1]) - coord_end + 1) / 3
+			else:	# normal
+				eq_coord.append(str(int(coord_start) + start_adjust))
+				eq_coord.append(str(int(coord_start) + start_adjust + end_adjust + 2))	# need to add two bp for rest of codon
+				orflen = (coord_end - int(coord_start) + 1) / 3
+		else:		# not ORFfinder output
+			orflen = 'unknown'
+			eq_coord = ['-', '-']
 
-		# convert the coordinates to match the envelope coords in terms of contig coords
-		eq_coord = []
-		actual_start = int(coord_start) + 1		# to correct for ORFfinder bug
-		actual_end = int(coord_end) + 1		# to correct for ORFfinder bug
-		start_adjust = (int(env_coord[0]) - 1) * 3
-		end_adjust = (int(env_coord[1]) - int(env_coord[0])) * 3
-		if actual_start < actual_end:		# positive strand
-			eq_coord.append(str(actual_start + start_adjust))
-			eq_coord.append(str(actual_start + start_adjust + end_adjust + 2))	# need to add two bp for rest of codon
-			orflen = (actual_end - actual_start + 1) / 3
-		else:					# negative strand
-			eq_coord.append(str(actual_start - start_adjust))
-			eq_coord.append(str(actual_start - start_adjust - end_adjust - 2))
-			orflen = (actual_start - actual_end + 1) / 3
+		# write the output
+		out_file.write('\t'.join([query, str(mlen), contig, str(orflen), str(evalue)]) +
+			'\t' + '\t'.join(hmm_coord) + '\t' + '\t'.join(env_coord) +
+			'\t' + '\t'.join(eq_coord) + '\n')
 
+		# record query
 		if query not in query_list:
 			query_list.append(query)
-			out_file.write(query + '\n')
-			out_file.write('\t'.join([contig, str(hit_num), str(domain_num)]) + '\t' + ' '.join(hmm_coord) + '\t' + str(mlen) + '\t' + ' '.join(env_coord) + '\t' + str(orflen) + '\t' + '\t'.join(eq_coord) + '\n')
-		else:
-			out_file.write('\t'.join([contig, str(hit_num), str(domain_num)]) + '\t' + ' '.join(hmm_coord) + '\t' + str(mlen) + '\t' + ' '.join(env_coord) + '\t' + str(orflen) + '\t' + '\t'.join(eq_coord) + '\n')
+
+	# third, sort and filter the query hits, if requested
+	if filter_hits:
+		with open(hmmer + '.parsed.filtered', 'w') as out_file2:
+			out_file2.write('Query\tORFs\n')
+			for query in query_list:
+				sublist = []
+				found = False
+				for capture in capture_hits:
+					if capture[0] == query:
+						found = True
+						sublist.append(capture)
+					elif found:		# assumes the query hits are all consecutive
+						break
+					else:
+						continue
+
+				# filter out hits with poor scores (> threshold)
+				filtered_sublist = [item for item in sublist if float(item[3]) <= threshold]
+				sublist = filtered_sublist
+				if len(sublist) < 1:
+					print('No hits passed filters for locus ' + str(query))
+					continue
+
+				# parse the hits for a query and determine most common contig, lowest E-value if ties
+				contigs = [item[2].split(':')[0] for item in sublist]
+				evalues = [float(item[3]) for item in sublist]
+
+				counts = []
+				contig_list = []
+				evalue_list = []
+				for contig in set(contigs):
+					counts.append(contigs.count(contig))
+					contig_list.append(contig)
+				if len([item for item in counts if item == max(counts)]) > 1:		# ties are present
+					indices = [index for index, item in enumerate(counts) if item == max(counts)]
+					top_contigs = [contig_list[index] for index in indices]
+					for contig in top_contigs:
+						evalue_list.append(evalues[contigs.index(contig)])	# just the first evalue
+					most_common = top_contigs[evalue_list.index(min(evalue_list))]
+				else:
+					most_common = max(set(contigs), key = contigs.count)
+
+				# keep hits for the most common contig (lowest E-value in ties)
+				indices = [index for index, item in enumerate(contigs) if item == most_common]
+				keep_hits = [sublist[index] for index in indices]
+
+				# order hits by HMMER profile order (first part of hmm_coord)
+				keep_hits.sort(key = lambda x: int(x[4][0]))
+
+				# write the query and comma-separated names of the ORFs to output, excluding dups
+				final_orfs = []
+				for hit in keep_hits:
+					if hit[2] not in final_orfs:
+						final_orfs.append(hit[2])
+				out_file2.write(query + '\t' + ','.join(final_orfs) + '\n')
