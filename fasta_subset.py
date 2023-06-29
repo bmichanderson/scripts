a0ecb96448f13185ce3e2d6c0e3baff1dbfa10e7
bmichanderson <banderson2914@gmail.com>
2023-06-29 18:29:35 +0800

    added option to match ID; cleaned up

--- fasta_subset.py
+++ fasta_subset.py
@@ -1,27 +1,31 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 #####################
 # Author: B. Anderson
 # Date: May 2021
-# Description: Retain a subset (-s list.txt) of fasta samples from a multifasta
+# Modified: June 2023
+# Description: Retain a subset (-s list.txt) of fasta samples from multifastas
 #####################
 
 
 import sys
+import os
 import argparse
 from Bio import SeqIO
 
 
 # instantiate the parser
 parser = argparse.ArgumentParser(description = 'A script to retain a subset (-s list.txt) of fasta samples from multifasta inputs.' +
-						' Output files will have the same name but starting with \"subset_\".' +
-						' NOTE: it is up to you to ensure the subset names only match once per multifasta.' +
-						' Also, the taxon names should be the 4th and 5th fields in your descriptions.')
+	' Output files will have the same name but starting with \"subset_\".' +
+	' NOTE: it is up to you to ensure the subset names only match once per multifasta.' +
+	' By default, the taxon names should be the 4th and 5th fields in the descriptions.' +
+	' To simply match the id, use \"-i yes\".')
 
 
 # add arguments to parse
 parser.add_argument('multifastas', type=str, nargs='+', help='The multifasta files to be filtered')
 parser.add_argument('-s', type=str, dest='subset', help='File (required) with names of samples to match EXACTLY in the multifastas')
+parser.add_argument('-i', type=str, dest='idmatch', help='Whether (\"yes\" or \"no\") to match the ID rather than the original design [default no]')
 
 
 # parse the command line
@@ -30,9 +34,12 @@ if len(sys.argv[1:]) == 0:		# no arguments
 	sys.exit(1)
 
 args = parser.parse_args()
-
 multifastas = args.multifastas
 subset = args.subset
+idmatch = args.idmatch
+
+if not idmatch:
+	idmatch = 'no'
 
 
 # capture list of files for filtering
@@ -46,7 +53,10 @@ if subset:
 	sample_list = []
 	with open(subset, 'r') as sub_list:
 		for line in sub_list:
-			sample_list.append(" ".join(line.rstrip().split()))
+			if idmatch == 'yes':
+				sample_list.append(line.strip())
+			else:
+				sample_list.append(" ".join(line.rstrip().split()))
 else:
 	parser.print_help(sys.stderr)
 	sys.exit(1)
@@ -56,10 +66,14 @@ else:
 for multifasta in file_list:
 	omit_count = 0
 	retained_count = 0
-	with open(multifasta, 'r') as infile, open('subset_' + multifasta, 'w') as outfile:
+	with open(multifasta, 'r') as infile, open('subset_' + os.path.basename(multifasta), 'w') as outfile:
 		fastas = SeqIO.parse(infile, 'fasta')
 		for fasta in fastas:
-			name = " ".join(fasta.description.split()[3:5])
+			if idmatch == 'yes':
+				name = fasta.id
+			else:
+				name = " ".join(fasta.description.split()[3:5])
+
 			if name in sample_list:
 				SeqIO.write(fasta, outfile, 'fasta')
 				retained_count = retained_count + 1
