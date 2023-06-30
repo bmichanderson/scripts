699cfc4a519eca6466ce72077286a1e9a0bf845b
bmichanderson <banderson2914@gmail.com>
2023-06-30 21:49:41 +0800

    major change in output; include loci report after main report; add threshold argument

--- hybrid_parents.py
+++ hybrid_parents.py
@@ -23,6 +23,9 @@ parser = argparse.ArgumentParser(description = 'A script to assess hybrid hetero
 # add arguments to parse
 parser.add_argument('alignments', type = str, nargs = '*', help = 'The multiple sequence alignment(s)')
 parser.add_argument('-f', type = str, dest = 'hybrids_file', help = 'The file with sample IDs of hybrids in the alignments (one per line)')
+parser.add_argument('-t', type = int, dest = 'thresh', help = 'The threshold (greater than or equal to) for minimum percent of ' +
+	'the total for reporting a top parent count [default 20]; e.g., only report if the top count is at least 20 percent of ' +
+	'the total number of ambiguities')
 
 
 # parse the command line
@@ -32,10 +35,13 @@ if len(sys.argv[1:]) == 0:		# if there are no arguments
 args = parser.parse_args()
 alignments = args.alignments
 hybrids_file = args.hybrids_file
+threshold = args.thresh
 
 if any([not alignments, not hybrids_file]):
 	parser.print_help(sys.stderr)
 	sys.exit(1)
+elif not threshold:
+	threshold = 20
 
 
 # define functions
@@ -93,7 +99,7 @@ def get_groups(ambiguities, alignment):
 		base1 = amb_dict[ambiguity[1]][0]
 		base2 = amb_dict[ambiguity[1]][1]
 		this_column = alignment[:, ambiguity[0]]		# slice all rows (samples), one position
-		# determine if the column includes both possible bases for the ambiguity, and no other similar ambiguities
+		# determine if the column includes both possible bases for the ambiguity
 		#if ambiguity[1] in set(this_column):		# found other identical ambiguities
 		#	continue
 		if all([base1 in set(this_column), base2 in set(this_column)]):
@@ -115,10 +121,8 @@ def get_groups(ambiguities, alignment):
 
 ## a function to get counts of parents occurring in opposite groups
 ## The input "parents" are a list of all possible parents to compare
-## The input "groups" are the various groupings to assess
-## (a list of lists each with two lists of parents)
-## It returns a list (sorted by count) of lists with parent combinations
-## and their counts
+## The input "groups" are the groupings to assess (a list of lists each with two lists of parents)
+## It returns a list (sorted by count) of lists with parent combinations and their counts
 def get_opposites(parents, groups):
 	output_list = []
 	for index, parent in enumerate(parents):
@@ -134,18 +138,61 @@ def get_opposites(parents, groups):
 					else:
 						continue
 				output_list.append([[parent, parent2], count])
-	# now sort the parents by how often they group separately
+	# now sort the parent combinations by how often they group separately
 	output_list.sort(key = lambda x: x[1], reverse = True)
 	return(output_list)
 
 
-## a function to create a report string
-def parent_report(parent_list):
-	if len(set(sum(parent_list, []))) > 10:		# more than 10 unique parents in top comparisons
-		report = 'more than 10 (' + str(len(set(sum(parent_list, [])))) + ')'
-	else:
-		report = '/'.join([(item[0] + ';' + item[1]) for item in parent_list])
-	return(report)
+## a function to create a parent result list
+## The six inputs are:
+## The hybrid ID, the filename of the alignment, the total number of ambiguities,
+## and lists for each of the top three scoring parent combos,
+## which include a list of the count and the combos (list of two samples) with that count
+## It returns a list with the following fields for reporting (12):
+## 'Hybrid', 'Alignment', 'Ambiguities', 'count1', 'Parent1a', 'Parent1b',
+## 'count2', 'Parent2a', 'Parent2b', 'count3', 'Parent3a', 'Parent3b'
+def parent_report(hybrid, alignment_name, total, parent1_list, parent2_list, parent3_list):
+	output_list = []
+	output_list.append(str(hybrid))
+	output_list.append(str(alignment_name))
+	output_list.append(str(total))
+
+	for parent_list in [parent1_list, parent2_list, parent3_list]:
+		output_list.append(str(parent_list[0]))		# the count
+
+		# determine if there are multiple combos
+		if len(parent_list[1]) > 1:
+			if len(set(sum(parent_list[1], []))) > 4:		# more than 4 unique parents
+				output_list.append('>4')
+				output_list.append('>4')
+			else:
+				parenta_list = []
+				parentb_list = []
+				for combo in parent_list[1]:
+					if any([combo[0] in parenta_list, combo[0] in parentb_list]):
+						if any([combo[1] in parenta_list, combo[1] in parentb_list]):
+							continue
+						elif combo[0] in parenta_list:
+							parentb_list.append(combo[1])
+						else:
+							parenta_list.append(combo[1])
+					elif combo[1] in parenta_list:
+						parentb_list.append(combo[0])
+					elif combo[1] in parentb_list:
+						parenta_list.append(combo[0])
+					else:
+						parenta_list.append(combo[0])
+						parentb_list.append(combo[1])
+
+				parenta_string = '/'.join(list(set(parenta_list)))
+				parentb_string = '/'.join(list(set(parentb_list)))
+				output_list.append(parenta_string)
+				output_list.append(parentb_string)
+		else:
+			output_list.append(str(parent_list[1][0][0]))
+			output_list.append(str(parent_list[1][0][1]))
+
+	return(output_list)
 
 
 
@@ -162,14 +209,26 @@ with open(hybrids_file, 'r') as infile:
 		hybrids.append(line.strip())
 
 
+# create a list to keep track of the results for each hybrid across loci
+# The idea is to retain:
+#	the hybrid ID
+#	the alignment name
+#	the number of ambiguities for that hybrid in the alignment
+#	the top count and parent combos if there are enough ambiguities to evaluate them
+#	the second and third counts and parent combos
+result_list = []
+
+
+# print the categories for the output to stdout
+print('\t'.join(['Hybrid', 'Alignment', 'Ambiguities', 'count1', 'Parent1a', 'Parent1b',
+	'count2', 'Parent2a', 'Parent2b', 'count3', 'Parent3a', 'Parent3b']))
+
+
 # read in each alignment, extract hybrid sequences, retain non-hybrid samples in a new alignment,
 # then process the hybrids and their ambiguities
 for alignment_file in alignments:
-	print('\nProcessing alignment ' + os.path.basename(alignment_file))
+	#print('\nProcessing alignment ' + os.path.basename(alignment_file))
 	sequences = process_alignment(AlignIO.read(open(alignment_file, 'r'), 'fasta'), hybrids)
-
-	print('\n' + '\t'.join(['Hybrid', 'Ambiguities', 'Top_count', '2nd_count', '3rd_count',
-		'Top_parents', '2nd_parents', '3rd_parents']))
 
 	# turn the non-hybrid sequences into an alignment
 	clean_alignment = MultipleSeqAlignment(sequences[1])
@@ -179,62 +238,115 @@ for alignment_file in alignments:
 
 	# for each hybrid, determine the parent groups for its valid ambiguities
 	# ambiguities are valid if there are samples carrying both bases in that column
-	# ambiguities are invalid if there is only one of the other bases (uninformative)
 	for hybrid_entry in hybrids_ambiguities:
 		# go through the hybrid's ambiguities and determine groups of parents
 		groups = get_groups(hybrid_entry[1], clean_alignment)
 
-		# determine how many parents in total
-		tally = []
-		for group in groups:
-			for parent_list in group:
-				tally = tally + parent_list
+		# collate parent samples into a list
+		parents = list(set(sum(sum(groups, []), [])))
 
-		parents = list(set(tally))
-
-		if len(parents) < 2:	# not enough
-			print('\t'.join([str(hybrid_entry[0]), str(len(hybrid_entry[1])), '-', '-', '-',
-				'(none)', '(none)', '(none)']))
+		if any([len(parents) < 2, len(hybrid_entry[1]) < 8]):	# too few parents or sites
+			result_list.append(parent_report(hybrid_entry[0], os.path.basename(alignment_file),
+				len(hybrid_entry[1]), ['-', [['-', '-']]], ['-', [['-', '-']]], ['-', [['-', '-']]]))
 			continue		# move on to the next hybrid and its ambiguities
 		else:
-			# get counts of parents occurring in opposite groups
+			# get counts of parent combos when they occur in opposite groups
 			opposite_count = get_opposites(parents, groups)
 
-			# now evaluate how often the parents group separately
+			# determine the top parent combos
 			count_vals = sorted(list(set([item[1] for item in opposite_count])), reverse = True)
-			max_count = count_vals[0]
-			top_parents = [item[0] for item in opposite_count if item[1] == max_count]
-			top_report = parent_report(top_parents)
+			count1 = count_vals[0]
 
-			if len(count_vals) > 1:
-				second_count = count_vals[1]
-				second_parents = [item[0] for item in opposite_count if item[1] == second_count]
-				second_report = parent_report(second_parents)
+			if (count1 / len(hybrid_entry[1])) * 100 < threshold:
+				result_list.append(parent_report(hybrid_entry[0], os.path.basename(alignment_file),
+					len(hybrid_entry[1]), ['-', [['-', '-']]], ['-', [['-', '-']]], ['-', [['-', '-']]]))
 			else:
-				second_count = 0
-				second_report = '(none)'
-
-			if len(count_vals) > 2:
-				third_count = count_vals[2]
-				third_parents = [item[0] for item in opposite_count if item[1] == third_count]
-				third_report = parent_report(third_parents)
-			else:
-				third_count = 0
-				third_report = '(none)'
-
-			# report
-			if len(hybrid_entry[1]) < 4:		# if there are few heterozygous sites for the hybrid
-				print('\t'.join([str(hybrid_entry[0]), str(len(hybrid_entry[1])), 'fewer than 4', '-', '-',
-					'(none)', '(none)', '(none)']))
-			else:
-				if max_count > len(hybrid_entry[1])/2:		# more than half the ambiguities support the top comparisons
-					top_count = str(max_count) + '*'
+				if len(count_vals) > 1:
+					count2 = count_vals[1]
 				else:
-					top_count = str(max_count)
+					count2 = 0
+				if len(count_vals) > 2:
+					count3 = count_vals[2]
+				else:
+					count3 = 0
 
-				print('\t'.join([str(hybrid_entry[0]), str(len(hybrid_entry[1])), top_count,
-					str(second_count), str(third_count), top_report, second_report, third_report]))
+				parents1 = [item[0] for item in opposite_count if item[1] == count1]
+				parents2 = [item[0] for item in opposite_count if item[1] == count2]
+				parents3 = [item[0] for item in opposite_count if item[1] == count3]
+
+				if len(parents3) > 0:
+					result_list.append(parent_report(hybrid_entry[0], os.path.basename(alignment_file),
+						len(hybrid_entry[1]), [count1, parents1], [count2, parents2], [count3, parents3]))
+				elif len(parents2) > 0:		# only two sets of parents
+					result_list.append(parent_report(hybrid_entry[0], os.path.basename(alignment_file),
+						len(hybrid_entry[1]), [count1, parents1], [count2, parents2], [count3, [['-', '-']]]))
+				else:	# only one set of parents
+					result_list.append(parent_report(hybrid_entry[0], os.path.basename(alignment_file),
+						len(hybrid_entry[1]), [count1, parents1], [count2, [['-', '-']]], [count3, [['-', '-']]]))
 
 
-# HOW TO EXTEND ACROSS ALIGNMENTS? PASS SCORES TO SETS OF PARENTS?
-# TOP BY SIMPLE COUNT OF LOCI (TOP in EACH, WHEN LESS THAN 10)?
+# output the result to stdout, first sorting by hybrid ID and then alignment name
+outlist = sorted(result_list, key = lambda x: (x[0], x[1]))
+for entry in outlist:
+	print('\t'.join(entry))
+
+
+# if there is more than one locus, filter the results to also report locus counts for top parents
+# This only reports parents that were unambiguously top (e.g., ignores results wih >4)
+if len(alignments) > 1:
+	print('\n\n')
+	print('\t'.join(['Hybrid', 'Loci', 'Loci_gt_thresh', 'count1', 'Parent1', 'count2',
+	'Parent2', 'count3', 'Parent3', 'count4', 'Parent4']))
+	for hybrid in hybrids:
+		numloci = 0
+		numloci_gt_thresh = 0
+		top_parents = []
+		counts = []
+		for result in result_list:
+			if result[0] == hybrid:
+				numloci = numloci + 1
+				if all(['-' not in result[3], int(result[2]) > 0]):
+					if (int(result[3]) / int(result[2])) * 100 >= threshold:
+						numloci_gt_thresh = numloci_gt_thresh + 1
+						if '>4' not in result[4]:
+							if '/' in result[4]:
+								top_parents.extend(result[4].split('/'))
+							else:
+								top_parents.append(result[4])
+							if '/' in result[5]:
+								top_parents.extend(result[5].split('/'))
+							else:
+								top_parents.append(result[5])
+
+		# now count the occurrences of each parent in top parents
+		for parent in set(top_parents):
+			counts.append([parent, top_parents.count(parent)])
+
+		counts.sort(key = lambda x: x[1], reverse = True)
+
+		# report the top four across all loci
+		if len(counts) > 0:
+			count1 = counts[0][1]
+			parent1 = counts[0][0]
+			count2 = '-'
+			parent2 = '-'
+			count3 = '-'
+			parent3 = '-'
+			count4 = '-'
+			parent4 = '-'
+
+			if len(counts) > 3:
+				count4 = counts[3][1]
+				parent4 = counts[3][0]
+
+			if len(counts) > 2:
+				count3 = counts[2][1]
+				parent3 = counts[2][0]
+
+			if len(counts) > 1:
+				count2 = counts[1][1]
+				parent2 = counts[1][0]
+
+			print('\t'.join([str(hybrid), str(numloci), str(numloci_gt_thresh), str(count1),
+				str(parent1), str(count2), str(parent2), str(count3), str(parent3),
+				str(count4), str(parent4)]))
