288107add6ccaa03844364815f9078ed95de6ff6
bmichanderson <banderson2914@gmail.com>
2023-10-19 11:41:04 +0800

    cleaned up and made more useable

--- depth_summary.R
+++ depth_summary.R
@@ -1,18 +1,19 @@
 
-
-########
+##########
 # Author: Ben Anderson
 # Date: Nov 2020
-# Description: Calculate summary stats for read depths from samtools (arg1)
-########
+# Modified: Oct 2023
+# Description: Calculate summary stats from samtools depth output (arg1)
+##########
 
 args <- commandArgs()
 depths_file <- args[6]
 
 
 # read in the depths file
-data <- read.table(depths_file, header=FALSE, sep='\t')
+data <- read.table(depths_file, header = FALSE, sep = "\t")
 
 
 # print a summary of the depths column
 summary(data$V3)
+print(paste0("Standard deviation: ", sd(data$V3)))
--- reform_cp.sh
+++ reform_cp.sh
@@ -3,27 +3,29 @@
 #################
 # Author: B. Anderson
 # Date: Nov 2020
-# Modified: Mar 2021, April 2021 (for more checking and flexibility when sequence is lower quality and hits are broken up)
+# Modified: Mar 2021, April 2021 (for more checking and flexibility when sequence is lower quality and hits are broken up),
+#	Oct 2023 (clean up and make more useable)
 # Description: Reform a chloroplast fasta assembly (single circle) for consistent start position and orientation of LSC and SSC
 #################
 
 
 # python scripts used for fasta manipulation
-extract_script="/home/banderson/scripts/fasta_extract.py"
-merge_script="/home/banderson/scripts/fasta_merging.py"
-reform_script="/home/banderson/scripts/reform_contig.py"
+extract_script=~/scripts/fasta_extract.py
+merge_script=~/scripts/fasta_merging.py
+reform_script=~/scripts/reform_contig.py
 
 
 # Define a help function for an incorrect or empty call
 help()
 {
-(	echo -e '\n''\t'"This script will reform a single chloroplast fasta file to set consistent start position and orientation of LSC and SSC"
-	echo -e '\t'"It requires NCBI tools (makeblastdb, blastn) and two references:"
-	echo -e '\t\t'"1 - a gene (preferably psbA) near the start of the LSC that normally has reverse orientation"
-	echo -e '\t\t'"2 - a gene (preferably ccsA) in the SSC that normally has forward orientation"
-	echo -e '\t'"It will create a new copy of the chloroplast fasta as \"new_cp.fasta\""'\n'
-	echo -e '\t'"Usage: reform_cp.sh cp.fasta ref1 ref2"'\n'
-	echo -e '\t'"WARNING: will remove temp*.out temp*.fasta db* among other files"'\n'
+(	echo -e "\n\tThis script will reform a single chloroplast fasta file to set consistent \
+		start position and orientation of LSC and SSC"
+	echo -e "\tIt requires NCBI tools (makeblastdb, blastn) and two references:"
+	echo -e "\t\t1 - a gene (preferably psbA) near the start of the LSC that normally has reverse orientation"
+	echo -e "\t\t2 - a gene (preferably ccsA) in the SSC that normally has forward orientation"
+	echo -e "\tIt will create a new copy of the chloroplast fasta as \"new_cp.fasta\"\n"
+	echo -e "\tUsage: reform_cp.sh cp.fasta ref1 ref2\n"
+	echo -e "\tWARNING: will remove temp*.out temp*.fasta db* among other files\n"
 ) 1>&2
 	exit 1
 }
@@ -41,12 +43,10 @@ ref1=$(readlink -f "$2")
 ref2=$(readlink -f "$3")
 
 
-## Step 1: determine the IR locations and set a start at the beginning of the LSC
-
+# Step 1: determine the IR locations and set a start at the beginning of the LSC
 cp "$chloroplast" temp_cp.fasta
 makeblastdb -in temp_cp.fasta -out db -dbtype nucl -logfile temp.log
 blastn -query temp_cp.fasta -db db -outfmt "6 std slen" | head > temp_blast.out
-
 len=$(awk ' NR==1 {print $13} ' temp_blast.out)
 
 # typically, the second largest hits are the IR, but it is good to check given the sequence may have errors or NNNs
@@ -79,10 +79,9 @@ fi
 irlen=$(awk -v "row=$ir_firstrow" ' NR==row {print $4} ' temp_blast.out)
 ir_identity=$(awk -v "row=$ir_firstrow" ' NR==row {print $3} ' temp_blast.out)
 
-
-# a: find largest possible IR (start may be in an IR) by setting a new contig at half length location and comparing blast results
-
-"$reform_script" temp_cp.fasta $(echo "$len / 2" | bc) && mv new_contig.fasta temp_cp2.fasta
+# a: find largest possible IR (start may be in an IR) by setting a new contig at
+# half length location and comparing blast results
+python3 "$reform_script" temp_cp.fasta $(echo "$len / 2" | bc) && mv new_contig.fasta temp_cp2.fasta
 makeblastdb -in temp_cp2.fasta -out db2 -dbtype nucl -logfile temp.log
 blastn -query temp_cp2.fasta -db db2 -outfmt 6 | head > temp_blast2.out
 
@@ -123,25 +122,17 @@ fi
 
 rm db*
 
-
 # b: find the locations of the IR boundaries and set start based on LSC
-
 irb_start=$(awk -v "row=$ir_firstrow" ' NR==row {print $7} ' temp_blast.out)
 irb_end=$(awk -v "row=$ir_firstrow" ' NR==row {print $8} ' temp_blast.out)
 ira_start=$(awk -v "row=$ir_firstrow" ' NR==row {print $10} ' temp_blast.out)
 ira_end=$(awk -v "row=$ir_firstrow" ' NR==row {print $9} ' temp_blast.out)
-
 
 # check whether the hits are in the right order
 if [ "$ira_start" -gt "$irb_start" ]; then
 	echo "Hits in the wrong order! Check the output" && cp temp_blast.out REVIEW_ME_blast3.out
 	exit 1
 fi
-#ira_start=$(awk ' NR==4 {print $7} ' temp_blast.out) && \
-#ira_end=$(awk ' NR==4 {print $8} ' temp_blast.out) && \
-#irb_start=$(awk ' NR==4 {print $10} ' temp_blast.out) && \
-#irb_end=$(awk ' NR==4 {print $9} ' temp_blast.out); fi
-
 
 # check if the piece between the IRs in this orientation is the LSC or SSC
 if [ $(echo "$irb_start - $ira_end" | bc) -gt $(echo "$len / 3" | bc) ]; then
@@ -152,15 +143,11 @@ else
 	new_start=$(echo "$irb_end + 1" | bc)
 fi
 
-
 if [ "$new_start" -ne 1 ]; then
-	"$reform_script" temp_cp.fasta "$new_start" && mv new_contig.fasta temp_cp.fasta
+	python3 "$reform_script" temp_cp.fasta "$new_start" && mv new_contig.fasta temp_cp.fasta
 fi
 
-
-
 # c: regenerate the database and set locations of IR boundaries
-
 makeblastdb -in temp_cp.fasta -out db -dbtype nucl -logfile temp.log
 blastn -query temp_cp.fasta -db db -outfmt 6 | head > temp_blast.out
 
@@ -205,9 +192,8 @@ if [ "$ira_start" -gt "$irb_start" ]; then
 fi
 
 
-## Step 2: extract the LSC; check orientation and location of ref1 in LSC; modify LSC as needed
-
-"$extract_script" -c 1..$(echo "$ira_start - 1" | bc) temp_cp.fasta && mv extract.fasta LSC.fasta
+# Step 2: extract the LSC; check orientation and location of ref1 in LSC; modify LSC as needed
+python3 "$extract_script" -c 1..$(echo "$ira_start - 1" | bc) temp_cp.fasta && mv extract.fasta LSC.fasta
 
 blastn -query "$ref1" -db db -outfmt 6 > temp_blast_ref1.out
 ref1_start=$(awk ' NR==1 {print $9} ' temp_blast_ref1.out)
@@ -219,9 +205,8 @@ elif [ "$ref1_end" -gt 1000 ]; then
 fi
 
 
-## Step 3: extract the SSC; check orientation of ref2 in SSC; modify SSC as needed
-
-"$extract_script" -c $(echo "$ira_end + 1" | bc)..$(echo "$irb_start - 1" | bc) temp_cp.fasta && mv extract.fasta SSC.fasta
+# Step 3: extract the SSC; check orientation of ref2 in SSC; modify SSC as needed
+python3 "$extract_script" -c $(echo "$ira_end + 1" | bc)..$(echo "$irb_start - 1" | bc) temp_cp.fasta && mv extract.fasta SSC.fasta
 
 blastn -query "$ref2" -db db -outfmt 6 > temp_blast_ref2.out
 ref2_start=$(awk ' NR==1 {print $9} ' temp_blast_ref2.out)
@@ -231,13 +216,12 @@ if [ "$ref2_end" -lt "$ref2_start" ]; then
 fi
 
 
-## Step 4: extract the IRs, stitch all back together and remove intermediate files
-
-"$extract_script" -c "$ira_start".."$ira_end" temp_cp.fasta && mv extract.fasta IRA.fasta
-"$extract_script" -c "$irb_start".."$irb_end" temp_cp.fasta && mv extract.fasta IRB.fasta
+# Step 4: extract the IRs, stitch all back together and remove intermediate files
+python3 "$extract_script" -c "$ira_start".."$ira_end" temp_cp.fasta && mv extract.fasta IRA.fasta
+python3 "$extract_script" -c "$irb_start".."$irb_end" temp_cp.fasta && mv extract.fasta IRB.fasta
 
 cat LSC.fasta IRA.fasta SSC.fasta IRB.fasta > new_cp.fasta
 
-"$merge_script" new_cp.fasta && mv merged.fasta new_cp.fasta
+python3 "$merge_script" new_cp.fasta && mv merged.fasta new_cp.fasta
 
 rm temp*.out temp*.fasta temp.log temp.perf db* IR* LSC.fasta SSC.fasta
