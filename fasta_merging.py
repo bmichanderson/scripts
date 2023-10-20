4d4f98cf231a05e56f9660725d379b082f4dfc8f
bmichanderson <banderson2914@gmail.com>
2023-10-20 14:57:46 +0800

    cleaned up

--- fasta_merging.py
+++ fasta_merging.py
@@ -1,17 +1,16 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 ##########################
 # Author: B. Anderson
 # Date: 18 May 2020
-# Modified: Nov 2020
+# Modified: Nov 2020, Oct 2023 (cleaned up)
 # Description: read a multifasta file and output a fasta file with the sequences merged together into one
 ##########################
 
 
+import sys
 import argparse
-import sys				# allows access to command line arguments
-#import re				# for use of regular expressions
-from Bio import SeqIO			# for reading and writing sequence records
+from Bio import SeqIO
 from Bio.Seq import Seq
 
 
@@ -26,12 +25,11 @@ parser.add_argument('-r', type=int, dest='repeats', help='Specify the number of 
 
 
 # parse the command line
-if len(sys.argv[1:]) == 0:              # if there are no arguments
-        parser.print_help(sys.stderr)
-        sys.exit(1)
+if len(sys.argv[1:]) == 0:		# if there are no arguments
+	parser.print_help(sys.stderr)
+	sys.exit(1)
 
 args = parser.parse_args()
-
 con = args.contigs
 spacer = args.spacer
 repeats = args.repeats
@@ -40,7 +38,7 @@ if not repeats:
 	repeats = 1
 
 if not spacer:
-        spacer_present = False
+	spacer_present = False
 else:
 	spacer_present = True
 	spacer = Seq(spacer*repeats)
@@ -50,24 +48,18 @@ else:
 fastas = []
 with open(con, 'r') as fasta_file, open('merged.fasta', 'w') as out_file:
 	for fasta in SeqIO.parse(fasta_file, 'fasta'):
-		#sequence = str(fasta.seq).replace('*', 'N')	# substitute for the strange * characters in some NOVOPlasty outputs (?)
-		#sequence = re.sub('\d', 'N', str(fasta.seq))	# substitute any digits in some NOVOPlasty outputs (?)
-		#fasta.seq = Seq(sequence)
 		if len(fasta.seq) > 0:
 			fastas.append(fasta)
 		else:
 			continue
 
 	concatenated = fastas[0]
-
 	if spacer_present:
 		concatenated.seq = concatenated.seq + spacer + spacer.join([x.seq for x in fastas[1:]])
 	else:
 		for fasta in fastas[1:]:
 			concatenated.seq = concatenated.seq + fasta.seq
-
 	concatenated.description = 'merged ' + concatenated.description
 	concatenated.id = 'merged'
 	concatenated.name = 'merged'
-
 	SeqIO.write(concatenated, out_file, 'fasta')
