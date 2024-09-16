b1c952512d6f54e738faea0cc610490b1eae3e6a
bmichanderson <banderson2914@gmail.com>
2024-09-16 21:34:28 +0800

    cleaned up and added message when called without args

--- fasta_splitting.py
+++ fasta_splitting.py
@@ -1,19 +1,26 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 ##########################
-# Author: B. Anderson
+# Author: B.M. Anderson
 # Date: 19 Oct 2018
-# Modified: Oct 2020
+# Modified: Oct 2020, Sep 2024
 # Description: read a multifasta file (arg1) and output a fasta file for each entry
 ##########################
 
 
-import sys			# allows access to command line arguments
-from Bio import SeqIO		# SeqIO is part of Biopython for parsing files
+from Bio import SeqIO
+import sys
 
 
+# check that a multifasta was provided
+if len(sys.argv[1:]) == 0:
+	print('This script needs a single argument (the multifasta file to split)')
+	sys.exit(1)
+
+
+# open the input file and write a fasta file for each entry
 with open(sys.argv[1], 'r') as fasta_file:
-	fasta_iter = SeqIO.parse(fasta_file, 'fasta')
-	for entry in fasta_iter:
-		with open('_'.join(entry.description.rstrip().split()) + '.fasta', 'w') as output_file:
-			SeqIO.write(entry, output_file, 'fasta')
+	for fasta in SeqIO.parse(fasta_file, 'fasta'):
+		outname = '_'.join(fasta.description.strip().split()) + '.fasta'
+		with open(outname, 'w') as outfile:
+			SeqIO.write(fasta, outfile, 'fasta')
