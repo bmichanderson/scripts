89ca0581f86d3a5327f938a057dc6915768481aa
bmichanderson <banderson2914@gmail.com>
2025-04-03 22:12:53 +0800

    cleaned up and made more resilient to file naming

--- genbank_to_fasta.py
+++ genbank_to_fasta.py
@@ -1,28 +1,41 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+
 ##########################
-# Author: B. Anderson
+# Author: B.M. Anderson
 # Date: 28 May 2020
-# Description: read a genbank file (arg1) and write a fasta
+# Modified: Apr 2025 (cleaned up and made more resilient to file naming)
+# Description: read a (multi)genbank file (arg1) and write a (multi)fasta
 ##########################
+
 
 import sys			# allows access to command line arguments
+import argparse		# for parsing arguments
+import os			# for interacting with the operating system and paths
 from Bio import SeqIO		# SeqIO is part of Biopython for parsing files
 
-def help():
-	print('This script will convert a (multi)genbank file (arg1) into a single (multi)fasta file')
-	print('')
 
-if len(sys.argv) != 2:		# print help if incorrect number of arguments provided
-	sys.exit(help())
+# instantiate the parser
+parser = argparse.ArgumentParser(description = 'Convert (multi)genbank file(s) into (multi)fasta file(s). ' +
+	'The output file(s) will share the same name as the input but with extension \".fasta\"')
 
 
-# check the extension on the genbank file
-filepath = sys.argv[1]
-if filepath.split('.')[-1] != '.gb':
-	filepath = ''.join(filepath.split('.')[:-1]) + '.gb'
+# add arguments to parse
+parser.add_argument('genbanks', type=str, help='The (multi)genbank files', nargs='*')
 
-# open the genbank, then write the fasta
-with open(sys.argv[1], 'r') as gbfile, open(filepath.replace('.gb', '.fasta'), 'w') as out_file:
-	records = SeqIO.parse(gbfile, 'genbank')
-	for record in records:
-		SeqIO.write(record, out_file, 'fasta')
+
+# parse the command line
+if len(sys.argv[1:]) == 0:			# if there are no arguments
+	parser.print_help(sys.stderr)
+	sys.exit(1)
+
+args = parser.parse_args()
+genbanks = args.genbanks
+
+
+# read in the genbank(s) and write to fasta(s)
+for genbank in genbanks:
+	prefix, extension = os.path.splitext(genbank)
+	with open(genbank, 'r') as gbfile, open(prefix + '.fasta', 'w') as outfile:
+		records = SeqIO.parse(gbfile, 'genbank')
+		for record in records:
+			SeqIO.write(record, outfile, 'fasta')
