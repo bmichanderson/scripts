536d875b7d8e54a4005afc6dbaa640b5e8dc1f45
bmichanderson <banderson2914@gmail.com>
2026-08-05 14:25:53 +0800

    added option for a categorical column to boxplot.R

--- boxplot.R
+++ boxplot.R
@@ -5,6 +5,7 @@
 # Description: plot default R stats boxplot(s) for column(s) of a tab-separated file
 #	The input file (first argument) should have a header for the title of each column
 #	The output will be named as the first "."-demarcated field of the input file name, with added suffix "_plot#.pdf"
+#	If there are categories in the first column (not data), indicate this with "-c y"
 ##########
 
 
@@ -13,7 +14,9 @@ help <- function(help_message) {
 	if (missing(help_message)) {
 		cat("A script to plot boxplot(s) for column(s) of a tab-separated file\n")
 		cat("The input file should have a header row with the title(s) of the column(s)\n")
-		cat("Usage: Rscript boxplot.R input_file\n")
+		cat("Option:\n")
+		cat("\t-c\tWhether the first column contains categories for splitting, \"y\" or \"n\" [default]\n")
+		cat("Usage: Rscript boxplot.R input_file [-c \"y\" or \"n\"]\n")
 	} else {
 		cat(help_message)
 	}
@@ -25,33 +28,96 @@ args <- commandArgs(trailingOnly = TRUE)
 if (length(args) == 0) {
 	stop(help(), call. = FALSE)
 } else {
-	input_file <- args[1]
+	catch_args <- vector("list")
+	extra <- 1
+	catch <- TRUE
+	categories <- "n"
+	for (index in seq_len(length(args))) {
+		if (args[index] == "-c") {
+			categories <- args[index + 1]
+			catch <- FALSE
+		} else {
+			if (catch) {
+				catch_args[extra] <- args[index]
+				extra <- extra + 1
+			} else {
+				catch <- TRUE
+			}
+		}
+	}
+}
+
+if (length(catch_args) < 1) {
+	stop(help("Missing input file!\n"), call. = FALSE)
+} else if (extra > 2) {
+	stop(help("Too many arguments\n"), call. = FALSE)
 }
 
 
 # Read the input file
+input_file <- catch_args[[1]]
 prefix <- strsplit(basename(input_file), "\\.")[[1]]
 mydata <- read.table(input_file, sep = "\t", header = TRUE)
 multiple <- FALSE
-if (ncol(mydata) > 1) {
-	multiple <- TRUE
+
+
+# Determine plot dimensions depending on whether there are multiple categories
+if (categories == "y") {
+	cats <- TRUE
+	number_cats <- length(unique(mydata[, 1]))
+	pwidth <- number_cats * 2
+	pheight <- 12
+	first_column <- 2
+	if (ncol(mydata) > 2) {
+		multiple <- TRUE
+	}
+} else if (categories == "n") {
+	cats <- FALSE
+	pwidth <- 4
+	pheight <- 12
+	first_column <- 1
+	if (ncol(mydata) > 1) {
+		multiple <- TRUE
+	}
+} else {
+	stop(help("Categories option specified incorrectly!\n"), call. = FALSE)
 }
 
 
-# for each column, plot a boxplot
-if (multiple) {
-	for (column in seq_len(ncol(mydata))) {
-		pdf(paste0(prefix, "_plot", column, ".pdf"), width = 4, height = 12)
-		boxplot(mydata[, column],
-			ylim = c(0, max(mydata[, column], na.rm = TRUE) * 1.05),
-			main = colnames(mydata)[column])
-		invisible(dev.off())
+# plot boxplot(s)
+if (cats) {
+	mylevels <- unique(mydata[, 1])
+	mydata[, 1] <- factor(mydata[, 1], levels = mylevels)
+	# define a function to plot the boxplot with categories
+	plot_boxplot <- function(dframe, column) {
+		boxplot(dframe[, column] ~ dframe[, 1],
+			ylim = c(0, max(dframe[, column], na.rm = TRUE) * 1.05),
+			main = colnames(dframe)[column],
+			ylab = "", xlab = "", xaxt = "n")
+		yrange <- max(dframe[, column], na.rm = TRUE) * 1.05
+		axis(1, at = seq_len(length(mylevels)), labels = FALSE)
+		text(seq_len(length(mylevels)), par("usr")[3] - (0.02 * yrange), srt = 45, adj = 1,
+		labels = mylevels, xpd = TRUE)
 	}
 } else {
-	column <- 1
-	pdf(paste0(prefix, "_plot.pdf"), width = 4, height = 12)
-	boxplot(mydata[, column],
-		ylim = c(0, max(mydata[, column], na.rm = TRUE) * 1.05),
-		main = colnames(mydata)[column])
+	# define a function to plot the boxplot without categories
+	plot_boxplot <- function(dframe, column) {
+		boxplot(dframe[, column],
+			ylim = c(0, max(dframe[, column], na.rm = TRUE) * 1.05),
+			main = colnames(dframe)[column])
+	}
+}
+
+if (multiple) {
+	iter <- 1
+	for (column in seq(first_column, ncol(mydata))) {
+		pdf(paste0(prefix, "_plot", iter, ".pdf"), width = pwidth, height = pheight)
+		plot_boxplot(mydata, column)
+		invisible(dev.off())
+		iter <- iter + 1
+	}
+} else {
+	pdf(paste0(prefix, "_plot.pdf"), width = pwidth, height = pheight)
+	plot_boxplot(mydata, first_column)
 	invisible(dev.off())
 }
